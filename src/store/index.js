04299150e89b587f546fa280f4c65b391b9b4b15
DaviD D <jdavid.martinez@gmail.com>
2021-07-09 07:18:27 -0500

    Secction  18 video 243 - Working with multiples Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,51 @@
-import {createStore} from 'redux';
 import { createSlice, configureStore} from '@reduxjs/toolkit';
 
 
-const inicialState = {counter: 0, showCounter: true};
+const initialCounterState = {counter: 0, showCounter: true};
 
 const counterSlice = createSlice({
     name: 'counter',
-    inicialState,
+    initialState: initialCounterState,
     reducers: {
-        increment (state) {
-            state.counter++;
-        },
-        decrement (state) {
-            state.counter--;
-        },
-        increase (state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter (state)  {
-            state.showCounter = !state.showCounter;
-        }
+      increment(state) {
+        state.counter++;
+      },
+      decrement(state) {
+        state.counter--;
+      },
+      increase(state, action) {
+        state.counter = state.counter + action.payload;
+      },
+      toggleCounter(state) {
+        state.showCounter = !state.showCounter;
+      }
     }
-});
+  });
+
+  const initialAuthState = {
+      isAuthenticated: false
+  }
+
+  const authSlice = createSlice({
+      name: 'authentication',
+      initialState: initialAuthState,
+      reducer: {
+          login (state) {
+              state.isAuthenticated = true;
+          },
+          logout (state) {
+              state.isAuthenticated = false;
+          }
+      }
+  })
 
 
 
-const store =  configureStore({
-    reducer: counterSlice.reducer
-});
-
-export const counterActions = counterSlice.actions;
-export default store;
+const store = configureStore({
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer},
+  });
+  
+  export const counterActions = counterSlice.actions;
+  export const authActions = authSlice.actions;
+  
+  export default store;
