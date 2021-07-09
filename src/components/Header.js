7ce5a8c03c770124499c9e55a5c45b3f7d053ea3
DaviD D <jdavid.martinez@gmail.com>
2021-07-09 08:48:33 -0500

    Secction  18 video 245 - Splitting Our Code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,13 @@ import { authActions } from "../store/index";
 import classes from "./Header.module.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
-  
-  const dispatch = useDispatch();
-
-  const logoutHandler = event => {
+  const logoutHandler = (event) => {
     event.preventDefault();
     dispatch(authActions.logout());
-  }
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
